9edf67fc29ad8d6b299d7bb66f243c2d1549ab88
ngknnn <namkhanh2903@gmail.com>
2024-12-27 00:14:06 +0700

    commit 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   //output: "export",
   // distDir:'dist',
-  basePath: isProd ? '/viettelandyou' : '',
-  assetPrefix: isProd ? '/viettelandyou' : '',
+  basePath: '/viettelandyou',
+  assetPrefix: '/viettelandyou' ,
   images: {
     remotePatterns: [
       {
